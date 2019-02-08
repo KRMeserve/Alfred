876c12ad690b3c03c464c645b4e7e58048578684
Kyle Meserve <KyleMeserve@Kyles-MacBook-Air.local>
2019-02-08 16:56:06 -0500

    made landing page a component and imported into app.js

--- alfred/src/App.js
+++ alfred/src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
+import LandingPage from './LandingPage'
 
 class App extends React.Component {
   render() {
     return (
-      <div className="landing-page">
-        <img src="./img/Monocle-Transparent-Image.png" className="alfred"></img>
-        <h1 className="title">Alfred, Your Trading Bot</h1>
-         <a className="collision">
-            Let's Trade
-        </a>
-      </div>
+      <LandingPage></LandingPage>
     );
   }
 }
---
+++ alfred/src/LandingPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class LandingPage extends React.Component {
+  render() {
+    return (
+      <div className="landing-page">
+        <img src="./img/Monocle-Transparent-Image.png" className="alfred"></img>
+        <h1 className="title">I'm Alfr3d, Your Trading Bot</h1>
+         <a className="collision">
+            Let's Trade
+        </a>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
