83a4ccd45810776b1076aed4b4b8ca36bd1db3ac
KRMeserve <krmeserve@gmail.com>
2019-06-19 22:39:30 -0400

    removed Create Account from Landing Page

--- alfred/src/LandingPage.js
+++ alfred/src/LandingPage.js
@@ -9,9 +9,6 @@ class LandingPage extends React.Component {
          <a className="collision" href="#" onClick={()=>{this.props.changePage('showLandingPage', 'showTradingPage');}}>
             Let's Trade
         </a>
-        <a className="collision" href="#" onClick={()=>{this.props.changePage('showLandingPage', 'showCAPage');}}>
-            Create Account
-        </a>
         <a className="collision" href="#" onClick={()=>{this.props.changePage('showLandingPage', 'showLoginPage');}}>
             Log In
         </a>
